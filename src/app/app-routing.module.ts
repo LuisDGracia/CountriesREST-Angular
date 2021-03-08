2cfd390a58090aaa9a0d699e9d3e4b5f194ec062
Zuzki <ldegrac071@hotmail.com>
2021-03-08 10:21:46 -0500

    Shared Module with navbar and footer Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountryComponent } from './countries/country/country.component';
+import { MainPageComponent } from './countries/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'countries', component: MainPageComponent },
+  { path: 'countries/:country', component: CountryComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'countries' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountriesModule } from './countries/countries.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CountriesModule
+    CountriesModule,
+    SharedModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
