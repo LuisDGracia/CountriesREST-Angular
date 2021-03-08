331409340616ef4d6696541a683e2e85306a7e40
Zuzki <ldegrac071@hotmail.com>
2021-03-08 10:43:22 -0500

    Added filters component

--- src/app/countries/components/cards/cards.component.ts
+++ src/app/countries/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Country } from './../../interface/country.interface';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+
+  @Input() countries: Country[]= []
 
   constructor() { }
 
--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CountryComponent } from './country/country.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { CardsComponent } from './cards/cards.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { FiltersComponent } from './components/filters/filters.component';
 
 
 
@@ -11,7 +12,8 @@ import { CardsComponent } from './cards/cards.component';
   declarations: [
     CountryComponent,
     MainPageComponent,
-    CardsComponent
+    CardsComponent,
+    FiltersComponent,
   ],
   imports: [CommonModule],
   exports: [
